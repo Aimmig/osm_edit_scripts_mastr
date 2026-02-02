e9c5593dda9b8d55068ec0c031abd3a187ae2ebd
Andre Immig <an_drimg@protonmail.com>
2026-02-02 22:23:09 +0100

    Add basic code

---
+++ edit_functions.py
@@ -0,0 +1,49 @@
+import re
+import inspect
+
+gensource = 'generator:source'
+genmethod = 'generator:method'
+ref = 'ref'
+refEEG = 'ref:EEG'
+wind = "wind"
+turbine = "wind_turbine"
+mantype = 'manufacturer:type'
+model = 'model'
+
+
+def edit_element_ref(tags):
+    if tags.get(gensource) != (wind):
+        print("source not matching nothing done")
+        return tags
+    if tags.get(genmethod) != (turbine):
+        print("method not matching nothing done")
+        return tags
+    if tags.get(ref) and re.match(r'E[-0-9a-zA-Z]{32}$', tags.get(ref)) and not refEEG in tags:
+        print("updating tag")
+        tags[refEEG] = tags.get(ref)
+        tags.pop(ref, None)
+        return tags
+    return tags
+
+
+def edit_element_man(tags, debug=False):
+    if tags.get(gensource) != (wind):
+        print("source not matching nothing done")
+        return tags
+    if tags.get(genmethod) != (turbine):
+        print("method not matching nothing done")
+        return tags
+    if mantype in tags and model in tags:
+        if tags[model] != tags[mantype]:
+            print("model and man:type both exist but missmatch. Nothing done")
+            return tags
+        else:
+            print("model was correct, remove manunfacturer:type")
+            tags.pop(mantype, None)
+            return tags
+    elif mantype in tags:
+        print("model didn't exist, renaming man:type -> model")
+        tags[model] = tags[mantype]
+        tags.pop(mantype, None)
+        return tags
+    return tags
---
+++ test_ref_and_man_edit.py
@@ -0,0 +1,43 @@
+import inspect
+from edit_functions import *
+
+def create_test_list():
+    w = {gensource:"winde",genmethod:turbine, mantype: 'E-111'} 
+    v = {gensource:wind,genmethod:"horizontal", ref: 'bla'} 
+    a = {gensource:wind,genmethod:turbine, model: 'blub'} 
+    b = {gensource:wind,genmethod:turbine, ref: 'Easdf0'} 
+    c = {gensource:wind,genmethod:turbine, ref: 'Vxxxxx', mantype: 'V137', model: 'V137'} 
+    d = {gensource:wind,genmethod:turbine, ref: 'E4183201RS50601064000066515800027'} 
+    e = {gensource:wind,genmethod:turbine, 'ref:eeg': 'Exxxxxxxx', mantype: 'V137XX', model: 'V137-YY'} 
+    f = {gensource:wind,genmethod:turbine, refEEG: 'Exxxxxxxx'} 
+    g = {gensource:wind,genmethod:turbine, refEEG: 'Exxxxxxxx', mantype: 'N175'} 
+    h = {gensource:wind,genmethod:turbine, refEEG: 'E4183201RS50434254000091798500009'} 
+    i = {genmethod:turbine, refEEG:'E4183201RS50434254000091798500009'} 
+    j = {genmethod:turbine, ref:'E4183201RS50434254000091798500009'} 
+    k = {gensource:wind, refEEG: 'E4183201RS50434254000091798500009'} 
+    l = {gensource:wind, ref: 'Vxxxxx'} 
+    test_list = [a, b, c, d, e, f, g, h, i, j, k, l, w, v]
+    return test_list
+
+def main():
+    #full_args = inspect.getfullargspec(edit_element_man)
+    #if 
+    #print(inspect.getfullargspec(edit_element_ref))
+    
+
+    print("TESTING ref -> ref:EEG -----------")
+    for x in create_test_list():
+        print(x)
+        y = edit_element_ref(x)
+        print(y)
+        print("----")
+    print("\n\n\n")
+    print("TESTING manufacturer:type -> model -----------")
+    for x in create_test_list():
+        print(x)
+        y = edit_element_man(x)
+        print(y)
+        print("----")
+
+main()
+
