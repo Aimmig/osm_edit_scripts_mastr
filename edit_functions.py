79e5a662739b35506d477bee92d9561a5295c106
Andre Immig <an_drimg@protonmail.com>
2026-02-02 22:46:21 +0100

    Prepare edit_element functions

--- edit_functions.py
+++ edit_functions.py
@@ -7,11 +7,13 @@ ref = 'ref'
 refEEG = 'ref:EEG'
 wind = "wind"
 turbine = "wind_turbine"
+solar = "solar"
+pv = "photovoltaic"
 mantype = 'manufacturer:type'
 model = 'model'
 
 
-def edit_element_ref(tags):
+def edit_element_ref_wind(tags):
     if tags.get(gensource) != (wind):
         print("source not matching nothing done")
         return tags
@@ -25,6 +27,19 @@ def edit_element_ref(tags):
         return tags
     return tags
 
+def edit_element_ref_pv(tags):
+    if tags.get(gensource) != (solar):
+        print("source not matching nothing done")
+        return tags
+    if tags.get(genmethod) != (photovoltaic):
+        print("method not matching nothing done")
+        return tags
+    if tags.get(ref) and re.match(r'E[-0-9a-zA-Z]{32}$', tags.get(ref)) and not refEEG in tags:
+        print("updating tag")
+        tags[refEEG] = tags.get(ref)
+        tags.pop(ref, None)
+        return tags
+    return tags
 
 def edit_element_man(tags, debug=False):
     if tags.get(gensource) != (wind):
---
+++ wind_ref_to_ref_eeg.py
@@ -0,0 +1,25 @@
+from osm_bot_abstraction_layer.generic_bot_retagging import run_simple_retagging_task
+from edit_functions import *
+
+def main():
+    run_simple_retagging_task(
+        max_count_of_elements_in_one_changeset=25,
+        objects_to_consider_query="""
+[out:xml][timeout:25000];
+area["name"="Deutschland"]->.boundaryarea;
+(
+  nwr(area.boundaryarea)["ref"~"^E[-0-9a-zA-Z]{32}"]["generator:source"="wind"]["generator:method"="wind_turbine"];
+);
+out body;
+>;
+out skel qt;
+""",
+        cache_folder_filepath='/tmp/',
+        is_in_manual_mode=True,
+        changeset_comment='Windanlagen in DE: ref -> ref:EEG, wenn Exx...xx',
+        discussion_url='https://community.openstreetmap.org/t/import-marktstammdatenregister-data-for-wind-power-plants/140622/10',
+        osm_wiki_documentation_page='https://wiki.openstreetmap.org/wiki/Mechanical_Edits/onterof_mastr_bot/migrate_some_ref_to_ref_eeg_solar_wind_germany',
+        edit_element_function=edit_element_ref_wind,
+    )
+
+main()
